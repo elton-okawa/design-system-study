7e889e3b2c603d6a9f9ee34cb872151112ee0ab2
Elton Okawa <elton.okawa@gmail.com>
2023-11-11 16:49:10 -0300

    chore: remove mui

--- src/theme/MuiTheme/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider } from '@mui/material';
-import React, { PropsWithChildren } from 'react';
-import { theme } from './theme';
-
-export const MuiTheme: React.FC<PropsWithChildren> = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
